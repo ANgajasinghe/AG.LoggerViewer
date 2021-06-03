7b4fb7a4660f81a434a912db3a3cb2521a7c52e0
ANgajasinghe <angajasinghe@gmail.com>
2021-06-04 03:26:12 +0530

    Add project files.

---
+++ AG.LoggerViewer.Application/Util/LoggerUtitlity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AG.LoggerViewer.Application.Util
+{
+    // Singleton class
+    public class LoggerUtitlity
+    {
+
+        public string LoggerPath { get; set; }
+
+    }
+}
---
+++ AG.LoggerViewer.UI/DependencyInjection.cs
@@ -0,0 +1,42 @@
+﻿using AG.LoggerViewer.Application.Commin;
+using AG.LoggerViewer.Application.Util;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AG.LoggerViewer.UI
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAGLogger(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddRazorPages();
+
+            var loggerUtility = configuration.GetSection("AGLoggerViewer").Get<LoggerUtitlity>();
+
+            if (loggerUtility == null) throw new AGLoggerExceptions("Cannot read logger utility, Please add {LoggerUtitlity} section");
+
+            services.AddSingleton(loggerUtility);
+
+
+
+            return services;
+
+        }
+
+        public static IApplicationBuilder UserAGLogger(this IApplicationBuilder app)
+        {
+            app.UseStaticFiles();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+
+            return app;
+        }
+    }
+}
